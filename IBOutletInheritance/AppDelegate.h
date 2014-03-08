039b04bdc6a61f12a04c1c01deb9b113cfbb7178
Abdullah Bakhach <abdullah.bakhach@gmail.com>
2014-03-08 08:16:11 +0200

    Initial Commit

---
+++ IBOutletInheritance/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IBOutletInheritance
+//
+//  Created by Ahmad Mehio on 3/8/14.
+//  Copyright (c) 2014 Ahmad Mehio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IBOutletInheritance/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IBOutletInheritance
+//
+//  Created by Ahmad Mehio on 3/8/14.
+//  Copyright (c) 2014 Ahmad Mehio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
